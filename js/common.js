a0ffd4a319fc5630e780b21065b705e0d2996388
EC2 Default User <ec2-user@ip-172-31-16-191.ap-northeast-1.compute.internal>
2021-04-08 13:43:37 +0000

    Create file

---
+++ js/common.js
@@ -0,0 +1,10 @@
+$(function(){
+  $('.carousel-list').slick({
+    autoplay:true,
+    autoplaySpeed: 3000,
+    dots: true,
+    slidesToShow: 3,
+    centerMode: true,
+    centerPadding: '100px',
+  });
+});
